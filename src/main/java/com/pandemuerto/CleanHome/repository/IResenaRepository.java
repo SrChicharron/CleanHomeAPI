d707350d0a7085daf69fe5484b771bde61083732
Moises Navarro <2015313073@uteq.edu.mx>
2023-08-15 04:12:51 -0600

    Funciones de reseña

--- src/main/java/com/pandemuerto/CleanHome/repository/IResenaRepository.java
+++ src/main/java/com/pandemuerto/CleanHome/repository/IResenaRepository.java
@@ -4,6 +4,11 @@ import com.pandemuerto.CleanHome.model.entity.Resena;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface IResenaRepository extends JpaRepository<Resena, Integer> {
+    List<Resena> findAllByEvaluadoId(int idEvaluado);
+    List<Resena> findAllByEvaluadorId(int idEvaluador);
+    List<Resena> findAllByPublicacionId(int idPublicacion);
 }
